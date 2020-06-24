f8457b7ec71abd08dec69662aa20ad451b52d856
THeMAN-nl <60950728+THeMAN-nl@users.noreply.github.com>
2020-06-23 21:19:01 -0400

    weather app upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import New from './New'
+
+ReactDOM.render(
+  <div>
+    
+  
+   <App />
+   
+  </div>,
+  document.getElementById('root')
+);
+
+
